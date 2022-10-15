a78849f75832cb8e6fdc96d78251fa3fcaa2837e
Chanwoongs <tkfkdcksdnd@daum.net>
2022-10-16 04:37:31 +0900

    Class to render 2D image to 3D scene

---
+++ DX_Doom/spriteclass.h
@@ -0,0 +1,64 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: spriteclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _SPRITECLASS_H_
+#define _SPRITECLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <directxmath.h>
+
+using namespace DirectX;
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "textureclass.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: SpriteClass
+////////////////////////////////////////////////////////////////////////////////
+class SpriteClass
+{
+private:
+	struct VertexType
+	{
+		XMFLOAT3 position;
+		XMFLOAT2 texture;
+	};
+
+public:
+	SpriteClass();
+	SpriteClass(const SpriteClass&);
+	~SpriteClass();
+
+	bool Initialize(ID3D11Device*, int, int, const WCHAR*);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, int);
+
+	int GetIndexCount();
+	ID3D11ShaderResourceView* GetTexture();
+
+private:
+	bool InitializeBuffers(ID3D11Device*);
+	void ShutdownBuffers();
+	bool UpdateBuffers(ID3D11DeviceContext*, int, int);
+	void RenderBuffers(ID3D11DeviceContext*);
+
+	bool LoadTexture(ID3D11Device*, const WCHAR*);
+	void ReleaseTexture();
+
+private:
+	ID3D11Buffer* m_vertexBuffer, * m_indexBuffer;
+	int m_vertexCount, m_indexCount;
+	TextureClass* m_Texture;
+	int m_spriteWidth, m_spriteHeight;
+	int m_previousPosX, m_previousPosY;
+};
+
+#endif
